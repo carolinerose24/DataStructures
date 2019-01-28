fc01e56b50aec3e63f0925a231a3768645a123bf
Caroline Jarman <carolinerosejarman@gmail.com>
2019-01-28 09:06:57 -0700

    Added start and main

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Jarman, Caroline on 1/28/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "This is the data structures app" << endl;
+}
---
+++ DataStructures/Controller/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  DataStructures
+//
+//  Created by Jarman, Caroline on 1/28/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include <iostream>
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+}
