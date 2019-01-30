098154c36a3cb96de6a604f690960234a846735d
Caroline Jarman <carolinerosejarman@gmail.com>
2019-01-30 09:40:21 -0700

    Added operators and methods

--- DataStructures/Model/Linear/Array.h
+++ DataStructures/Model/Linear/Array.h
@@ -72,5 +72,61 @@ Array<Type> :: ~Array()
     delete [] internalArray;
 }
 
+template <class Type> //Operators
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign != this)
+    {
+        if (size != toAssign.getSize())
+        {
+            delete [] internalArray;
+            size = toAssign.getSize();
+            internalArray = new Type [size];
+        }
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return *this;
+}
+
+template <class Type> //another operator?
+Type & Array<Type> :: operator [] (int index)
+{
+    assert (index >= 0 && index < size);
+    return internalArray[index];
+}
+
+template <class Type>
+Type Array<Type> :: operator [] (int index) const
+{
+    assert (index >= 0 && index < size);
+    return internalArray[index];
+}
+
+template <class Type> //methods: ---------
+int Array<Type> :: getSize() const
+{
+    return size;
+}
+
+template <class Type>
+Type Array<Type> :: getFromIndex(int index)
+{
+    assert (index >= 0 && index < size);
+    
+    Type value = internalArray[index];
+    
+    return value;
+}
+
+template <class Type>
+void Array<Type> :: setAtIndex(int pos, Type item)
+{
+    assert(pos >= 0 && pos < size);
+    internalArray[pos] = item;
+}
 
 #endif /* Array_h */
