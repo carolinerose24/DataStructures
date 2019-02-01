6a0338b00331157fe1495704a33faf7e33ea4a3e
Caroline Jarman <carolinerosejarman@gmail.com>
2019-02-01 07:59:55 -0700

    Added ABC class code

---
+++ DataStructures/Model/Nodes/List.h
@@ -0,0 +1,31 @@
+//
+//  List.h
+//  DataStructures
+//
+//  Created by Jarman, Caroline on 2/1/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
+
+class List
+{
+protected:
+    int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+
+#endif /* List_h */
