062573e97e884f766a7ba9535997f8cc7e773292
Caroline Jarman <carolinerosejarman@gmail.com>
2019-02-07 08:34:00 -0700

    Added template stuff to list.h

--- DataStructures/Model/Linear/List.h
+++ DataStructures/Model/Linear/List.h
@@ -9,6 +9,26 @@
 #ifndef List_h
 #define List_h
 
+#include "../Nodes/LinearNodes.hpp"
+
+template <class Type>
+class List
+{
+protected: //sub classes, not external classes can see it
+    int size;
+public:
+    //Structure --> zero as a placeholder/abstract
+    //have code/algorithms
+    virtual void add(Type item) = 0; //virtual--> going to be overridden
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper -----Getters - not setters
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    
+};
 
 
 
