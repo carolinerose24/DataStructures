a4f77526e9774cdf7ad87ec145e03bf77331a4b1
Caroline Jarman <carolinerosejarman@gmail.com>
2019-02-11 09:26:03 -0700

    Added Queue hpp

---
+++ DataStructures/Model/Linear/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  DataStructures
+//
+//  Created by Jarman, Caroline on 2/11/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+
+#endif /* Stack_h */
